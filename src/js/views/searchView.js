98c638b4824fc12c151ded05784c94f680a74deb
Matthew Denune <mattdenune@gmail.com>
2020-02-05 14:00:10 -0500

    updated searchView.js

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -10,15 +10,30 @@ export const clearResults = () => {
     elements.searchResList.innerHTML = '';
 };
 
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+    if (title.length > limit) {
+        title.split(' ').reduce((acc, cur) => {
+           if (acc + cur.length <= limit) {
+                newTitle.push(cur);
+           }
+           return acc + cur.length
+        }, 0);
+        
+        return `${newTitle.join(' ')} ...`
+    } 
+    return title;
+};
+
 const renderRecipe = (recipe) => {
     const markup = `
         <li>
             <a class="results__link" href="#${recipe.recipe_id}">
                 <figure class="results__fig">
                     <img src="${recipe.image_url}" alt="${recipe.title}">
-                </figure>
+                </figure>   
                 <div class="results__name">
-                    <h4 class="results__name">${recipe.title}</h4>
+                    <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                     <p class="results__author">${recipe.publisher}</p>
                 </div>
             </a>
