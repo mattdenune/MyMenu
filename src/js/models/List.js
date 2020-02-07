0e3576d0dd39f8a88c0caaaa09561906e64e30f5
Matthew Denune <mattdenune@gmail.com>
2020-02-07 12:29:20 -0500

    installed Uniqid and created List model

---
+++ src/js/models/List.js
@@ -0,0 +1,21 @@
+import uniqid from 'uniqid';
+
+export default class List {
+    constructor() {
+        this.items= [];
+    };
+
+    addItem (count, unit, ingredient) {
+        const item = {
+            id: uniqid(),
+            count,
+            unit,
+            ingredient
+        }
+
+        deleteItem (id) {
+            this.items.splice()
+        }
+
+    };
+};
